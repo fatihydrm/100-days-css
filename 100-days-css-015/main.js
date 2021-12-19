6cae910f67a1a85bb89164c95a287c19f96bcd19
fatihydrm <karamsarf@outlook.com>
2021-12-20 02:00:11 +0300

    .

---
+++ 100-days-css-015/main.js
@@ -0,0 +1,26 @@
+document.querySelector("#file").onchange = function () {
+  $fileName = document.querySelector("#file");
+  $fileName.style.color = "#aaa";
+
+  $image= document.querySelector("#img");
+  $image.style.opacity = "0";
+
+  $btn = document.querySelector("#btn");
+  $btn.onclick = function () {
+    $line = document.querySelector("#line");
+    line.style.transform = "translate(0,-14px)";
+
+    $fileName = document.querySelector("#file");
+    $fileName.style.color = "transparent";
+
+    $label = document.querySelector("#label");
+    $label.style.border = "1px solid transparent";
+
+    $circle = document.querySelector("#circle");
+    $circle.style.opacity = "1";
+    $circle.style.animation = "cirlce 2.5s linear forwards";
+
+    $ok = document.querySelector("#ok");
+    $ok.style.animation = "ok 2.5s linear forwards";
+  }
+};
